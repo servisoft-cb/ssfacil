9499a49f7673ff9bdff42423e4ff0c8094446932
cleomarkfp <45854561+cleomarkfp@users.noreply.github.com>
2020-08-17 10:49:53 -0300

    Update uUtilPadrao.pas

--- uUtilPadrao.pas
+++ uUtilPadrao.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Classes, SysUtils, Dialogs, Variants, Forms, ShellApi, Windows, StrUtils, SqlExpr, DmdDatabase, DBClient, Controls, SMDBGrid,
-  DB, UEscolhe_Filial, Printers, Messages, DmdDatabase_NFeBD, StdCtrls, ExtCtrls;
+  DB, UEscolhe_Filial, Printers, Messages, DmdDatabase_NFeBD, StdCtrls, ExtCtrls, MaskUtils;
 
   function Monta_Numero(Campo: String; Tamanho: Integer): String;
   function TirarAcento(texto: string): string;
@@ -136,6 +136,7 @@ uses
 
   function fnc_Cliente_Estoque(ID_Pedido : Integer) : String;
 
+  function FormatarTelefone(Telefone : String):String;
 
 var
   Enter,Esc,F1,F2,F3,F4,F5,F6,F7,F8,F9,F10,F11,F12,Q: Word;
@@ -2653,6 +2654,54 @@ begin
   finally
     FreeAndNil(sds);
   end;
+end;
+
+
+function FormatarTelefone(Telefone : String):String;
+  function SomenteNumero(snum : String) : String;
+  VAR s1, s2: STRING;
+    i: Integer;
+  BEGIN
+    s1 := snum;
+    s2 := '';
+    FOR i := 1 TO Length(s1) DO
+      IF s1[i] IN ['0'..'9'] THEN
+        s2 := s2 + s1[i];
+    result := s2;
+  End;
+var sTel : String;
+    bZero : Boolean;
+    iDigitos : Integer;
+begin
+   //Obs: mascara prevê tratamento apenas para números brasileiros
+   //Obs2: Esta função não leva em conta o código do país (Ex: 55, ou +55)
+   sTel := SomenteNumero(Telefone); //Remove qualquer formatação que o usuário possa ter colocado.
+   if sTel='' then
+    Result := ''
+   else
+   begin
+     if sTel[1]='0' then //Verifica se foi adicionado o 0 no início do número
+     begin
+       bZero:= True;
+       sTel := Trim( copy(sTel,2,Length(sTel)) ); //Remove para fazer a formatação depois adiciona
+     end
+     else
+       bZero := False;
+     iDigitos := Length(sTel);
+     //Formata de acordo com a quantidade de números encontrados.
+     case iDigitos of
+       8 : Result := FormatMaskText('9999-9999;0;_',sTel); //8 digitos SEM DDD (ex: 34552318)
+       9 : Result := FormatMaskText('9 9999-9999;0;_',sTel); //9 digitos SEM DDD (ex: 991916889)
+      10 : Result := FormatMaskText('(99) 9999-9999;0;_',sTel); //8 Digitos (convencional, ex: 7734552318)
+      11 : Result := FormatMaskText('(99) 9 9999-9999;0;_',sTel); //9 Digitos (novos números, ex: 77991916889)
+      12 : Result := FormatMaskText('99(99)9999-9999;0;_',sTel); //Se foram 12 digitos possívelmente digitou a operadora também
+      13 : Result := FormatMaskText('99(99)9 9999-9999;0;_',sTel); //Se foram 13 digitos possívelmente digitou a operadora também
+     else
+       Result := Telefone; //Mantém na forma que o usuário digitou
+     end;
+     if bZero then //Para ficar com a preferência do usuário, se ele digitou o "0" eu mantenho.
+       Result := '0'+Result;
+   end;
 end;
 
 end.
